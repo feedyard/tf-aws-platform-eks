cee24e4bc9914930d5517a554e79ef9933d63f80
Nic Cheneweth <nchenewe@thoughtworks.com>
2019-02-03 16:16:15 -0600

    updating naming scheme

--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -1,28 +1,3 @@
-terraform {
-  required_version = ">= 0.11.11"
-}
-
-provider "aws" {
-  version = "~> 1.57"
-  region  = "${var.cluster_region}"
-}
-
-provider "random" {
-  version = "~> 2.0"
-}
-
-provider "local" {
-  version = "~> 1.1"
-}
-
-provider "null" {
-  version = "~> 2.0"
-}
-
-provider "template" {
-  version = "~> 2.0"
-}
-
 module "vpc" {
   source = "github.com/feedyard/tf-aws-platform-vpc?ref=2.0.1"
 
@@ -41,26 +16,10 @@ module "vpc" {
   }
 }
 
-data "aws_vpc" "ci_vpc" {
-  tags = {
-    Cluster = "${var.cluster_name}"
-  }
-}
-
-data "aws_subnet_ids" "cluster_internal" {
-  vpc_id = "${data.aws_vpc.ci_vpc.id}"
-
-  tags = {
-    Tier = "Internal"
-  }
-}
-
 module "eks" {
   source = "../.."
 
-  cluster_name       = "${var.cluster_name}"
-  cluster_subnet_ids = ["${data.aws_subnet_ids.cluster_internal.ids}"]
-  cluster_vpc_id     = "${data.aws_vpc.ci_vpc.id}"
+  cluster_name = "${var.cluster_name}"
 
   tags {
     "test"     = "terraform module continuous integration testing"
--- test/fixture/variables.tf
+++ test/fixture/variables.tf
@@ -1,3 +1,28 @@
+terraform {
+  required_version = ">= 0.11.11"
+}
+
+provider "aws" {
+  version = "~> 1.57"
+  region  = "${var.cluster_region}"
+}
+
+provider "random" {
+  version = "~> 2.0"
+}
+
+provider "local" {
+  version = "~> 1.1"
+}
+
+provider "null" {
+  version = "~> 2.0"
+}
+
+provider "template" {
+  version = "~> 2.0"
+}
+
 variable "cluster_region" {}
 
 variable "cluster_name" {}
