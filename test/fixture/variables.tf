314f41f05e553e93f771a4bf1690bd7fba13c801
Nic Cheneweth <nchenewe@thoughtworks.com>
2019-02-03 21:58:58 -0600

    two-phase integration test

--- test/fixture/variables.tf
+++ test/fixture/variables.tf
@@ -24,14 +24,4 @@ provider "template" {
 }
 
 variable "cluster_region" {}
-
 variable "cluster_name" {}
-variable "cluster_vpc_name" {}
-
-variable "cluster_cidr_reservation_start" {}
-
-variable "cluster_azs" {
-  type = "list"
-}
-
-variable "cluster_enable_nat_gateway" {}
