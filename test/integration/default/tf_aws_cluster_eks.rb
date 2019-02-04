b9a0acd8cd1f3c598d92517935ed286704142a8e
Nic Cheneweth <nchenewe@thoughtworks.com>
2019-02-04 16:58:06 -0600

    terating

--- test/integration/default/tf_aws_cluster_eks.rb
+++ test/integration/default/tf_aws_cluster_eks.rb
@@ -6,20 +6,17 @@ require 'json'
 tfvars = JSON.parse(File.read('./' + ENV['PLATFORM_ENV'] + '.json'))
 
 describe command('aws eks describe-cluster --name ' + tfvars['cluster_name']) do
-  its('stdout') { should include ("\"name\": \"" + tfvars['cluster_name']) }        # exist
-  its('stdout') { should include ("\"version\": \"" + tfvars['cluster_version']) }  # have correct version
-  its('stdout') { should include ("\"vpcId\": \"" + tfvars['cluster_vpc_id']) }     # be attached to cluster_vpc_id
-
-  its('stdout') { should include ("\"status\": \"ACTIVE\"") }                       # be active
+  its('stdout') { should include ("\"name\": \"" + tfvars['cluster_name']) }          # exist
+  its('stdout') { should include ("\"version\": \"" + "1.11") }    # have correct version, if specified
+  its('stdout') { should include ("\"status\": \"ACTIVE\"") }                         # be active
 end
 
 describe command('aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names ' + tfvars['cluster_name'] + '-default-eks-asg') do
-  its('stdout') { should include ("\"AutoScalingGroupName\": \"" + tfvars['cluster_name'] + '-default-eks-asg') }                             # exist
-  its('stdout') { should include ("\"LaunchConfigurationName\": \"" + tfvars['cluster_name'] + '-default-launch-config') }  # have correct launch config
+  its('stdout') { should include ("\"AutoScalingGroupName\": \"" + tfvars['cluster_name'] + '-default-eks-asg') }   # exist
+  its('stdout') { should include ("\"LaunchConfigurationName\": \"" + tfvars['cluster_name'] + '-default') }        # have correct launch config
 end
 
-describe command('aws autoscaling describe-launch-configurations --launch-configuration-names ' + tfvars['cluster_name'] + '-default-launch-config') do
-  its('stdout') { should include ("\"LaunchConfigurationName\": \"" + tfvars['cluster_name'] + '-default-launch-config') }  # exit
-  its('stdout') { should include ("\"InstanceType\": \"" + tfvars['work_instance'] + '-default-launch-configuration') }
+describe command('aws autoscaling describe-launch-configurations --launch-configuration-names ' + tfvars['cluster_name'] + '-default') do
+  its('stdout') { should include ("\"LaunchConfigurationName\": \"" + tfvars['cluster_name'] + '-default') }  # exist
+  its('stdout') { should include ("\"InstanceType\": \"" + "t2.xlarge") }                                     # have correct default instance
 end
-
